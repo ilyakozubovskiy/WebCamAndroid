66b28a29edf9084cee30d20030cb0e16bfbddc87
ilyak <ilyak@ASUS-TUF>
2022-04-12 18:41:14 +0300

    Add project files.

---
+++ WebCamAndroid/WebCamAndroid/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using Xamarin.Forms;
+
+namespace WebCamAndroid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            image.Source = ImageSource.FromFile("mount.jpg");
+            label.HorizontalTextAlignment = TextAlignment.Center;
+            label.Text = "Waiting for images from Web-camera";
+            Image_Loaded();
+        }
+        private async void Image_Loaded()
+        {
+            using (UdpClient client = new UdpClient(48654))
+            {
+                double size = 0;
+                while (true)
+                {
+                    var data = await client.ReceiveAsync();
+                    MemoryStream ms = new MemoryStream(data.Buffer);
+                    image.Source = ImageSource.FromStream(() => ms);
+                    size += data.Buffer.Length / 1048576d;
+                    label.HorizontalTextAlignment = TextAlignment.Start;
+                    label.Text = $"Bytes recieved: {size:0.0}";
+                }
+            }
+        }
+    }
+}
